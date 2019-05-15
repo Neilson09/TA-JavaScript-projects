8eaf61131ee04ee5684eb15746fd612f49019967
Cindy Neilson <cindy.neilson@gmail.com>
2019-05-14 20:58:42 -0700

    extensive updates

--- pizza/html.js
+++ pizza/html.js
@@ -1,3 +1,49 @@
+
+
+function getReceipt() {
+	var text1 = "<h3>You Ordered:</h3>;
+	var runningTotal = 0;
+	var sizeTotal = 0;
+	var sizeArray = document.getElementsByClassName("size");
+}
+
+
+
+for (var i = 0; i < sizeArray.length; i++) {
+	if (sizeArray[i].checked) {
+		var selectedSize = sizeArray[i].value;
+		text1 = text1+selectedSize+"<br>";
+	}
+}
+
+
+if (selectedSize === "Personal Pizza") {
+	sizeTotal = 6;
+} else if (selectedSize === "Medium Pizza") {
+	sizeTotal = 10;
+} else if (selectedSize === "Large Pizza") {
+	sizeTotal = 14;
+} else if (selectedSize === "Extra Large Pizza") {
+	sizeTotal = 16;
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var txt1 = document.getElementById("txt1");
 var txt2 = document.getElementById("txt2");
 var total = document.getElementById("total");
