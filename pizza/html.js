000dcf774e313861cdced0e921cc591141c3915a
Cindy Neilson <cindy.neilson@gmail.com>
2019-05-30 00:58:13 -0700

    EOD

--- pizza/html.js
+++ pizza/html.js
@@ -1,49 +1,53 @@
-var text = "Confirm your order:";
-var runningTotal = 0;
-var sizeTotal = 0;
-var cheeseTotal = 0;
-var selectedSize = document.querySelector('input[name="size"]:checked').value;
-if (selectedSize === "personal pizza") {
-  sizeTotal = 6;
-} else if (selectedSize === "medium pizza") {
-  sizeTotal = 10;
-} else if (selectedSize === "large pizza") {
-  sizeTotal = 14;
-} else if (selectedSize === "extra large pizza") {
-  sizeTotal = 16;
-}
-text = text + " " + " " + selectedSize; // + ", $" + sizeTotal + ".00";
-runningTotal = sizeTotal;
-var selectedCrust = document.querySelector('input[name="crust"]:checked').value;
-text = text + " with " + selectedCrust + ", ";
-var selectedSauce = document.querySelector('input[name="sauce"]:checked').value;
-text = text + selectedSauce + ", ";
-var selectedCheese = document.querySelector('input[name="cheese"]:checked')
-  .value;
-if (selectedCheese === "extra cheese") {
-  cheeseTotal = 3;
-}
-text = text + selectedCheese + ", and ";
-runningTotal = runningTotal + cheeseTotal;
-var toppingsTotal = 0;
-var toppingsArray = document.getElementsByClassName("toppings");
-var selectedToppings = [];
-for (var i = 0; i < toppingsArray.length; i++) {
-  if (toppingsArray[i].checked) {
-    selectedToppings.push(toppingsArray[i].value);
-    text = text + selectedToppings + " ";
+function total() {
+  var text = "Confirm your order:" + "<br />";
+  var runningTotal = 0;
+  var sizeTotal = 0;
+  var cheeseTotal = 0;
+  var selectedSize = document.querySelector('input[name="size"]:checked').value;
+  if (selectedSize === "personal pizza") {
+    sizeTotal = 6;
+  } else if (selectedSize === "medium pizza") {
+    sizeTotal = 10;
+  } else if (selectedSize === "large pizza") {
+    sizeTotal = 14;
+  } else if (selectedSize === "extra large pizza") {
+    sizeTotal = 16;
   }
-}
-var toppingsCount = selectedToppings.length;
-if (toppingsCount > 1) {
-  toppingsTotal = toppingsCount - 1;
-} else {
-  toppingsTotal = 0;
-}
-runningTotal = runningTotal + toppingsTotal;
-text = text + "TOTAL: $" + runningTotal + ".00";
-document.getElementByClassName("total").innerHTML = text;
+  text = text + " " + " " + selectedSize + "<br />"; // + ", $" + sizeTotal + ".00";
+  runningTotal = sizeTotal;
+  var selectedCrust = document.querySelector('input[name="crust"]:checked')
+    .value;
+  text = text + selectedCrust + "<br />";
+  var selectedSauce = document.querySelector('input[name="sauce"]:checked')
+    .value;
+  text = text + selectedSauce + "<br />";
+  var selectedCheese = document.querySelector('input[name="cheese"]:checked')
+    .value;
+  if (selectedCheese === "extra cheese") {
+    cheeseTotal = 3;
+  }
+  text = text + selectedCheese + "<br />";
+  runningTotal = runningTotal + cheeseTotal;
 
+  var toppingsTotal = 0;
+  var toppingsArray = document.getElementsByClassName("toppings");
+  var selectedToppings = [];
+  for (var i = 0; i < toppingsArray.length; i++) {
+    if (toppingsArray[i].checked) {
+      selectedToppings.push(toppingsArray[i].value);
+    }
+  }
+  text = text + selectedToppings + "<br />";
+  var toppingsCount = selectedToppings.length;
+  if (toppingsCount > 1) {
+    toppingsTotal = toppingsCount - 1;
+  } else {
+    toppingsTotal = 0;
+  }
+  runningTotal = runningTotal + toppingsTotal;
+  text = text + "TOTAL: $" + runningTotal + ".00";
+  document.getElementById("Order").innerHTML = text;
+}
 /*
   document.getElementById("showText").innerHTML = text;
   document.getElementById("totalPrice").innerHTML =
